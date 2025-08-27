2bd4971c5f6e867f95e02b54eb429cad3ffaa64c
Vanya Usalko <dk4aol@gmail.com>
2025-08-27 08:12:55 +0300

    # There is the fix for compilation issue.

--- tests/test_runner.c
+++ tests/test_runner.c
@@ -1,9 +1,13 @@
 #include <CUnit/Basic.h>
 #include <CUnit/CUnit.h>
+#include "exercises.h"
 
 // Объявления тестовых наборов
 extern CU_TestInfo exercise001_tests[];
 extern CU_TestInfo exercise002_tests[];
+
+int exercise001_tests_count = 1;
+int exercise002_tests_count = 1;
 
 int main() {
     // Инициализация CUnit
@@ -12,21 +16,32 @@ int main() {
     }
 
     // Регистрация тестовых наборов
-    CU_register_suites((CU_SuiteInfo[]){
-        {
-            .pName = "1. Print name, DOB, and mobile number",
-            .pInitFunc = NULL,
-            .pCleanupFunc = NULL,
-            .pTests = exercise001_tests
-        },
-        {
-            .pName = "2. Get the C version in use",
-            .pInitFunc = NULL,
-            .pCleanupFunc = NULL,
-            .pTests = exercise002_tests
-        },
-        CU_SUITE_INFO_NULL
-    });
+
+    CU_pSuite p_suite1 = CU_add_suite("1. Print name, DOB, and mobile number", NULL, NULL);
+    for (int i = 0; i < exercise001_tests_count; i++) {
+        CU_add_test(p_suite1, exercise001_tests[i].pName, exercise001_tests[i].pTestFunc);
+    }
+        
+    CU_pSuite p_suite2 = CU_add_suite("2. Get the C version in use", NULL, NULL);
+    for (int i = 0; i < exercise002_tests_count; i++) {
+        CU_add_test(p_suite2, exercise002_tests[i].pName, exercise002_tests[i].pTestFunc);
+    }
+
+    // CU_register_suites((CU_SuiteInfo[]){
+    //     {
+    //         .pName = "1. Print name, DOB, and mobile number",
+    //         .pInitFunc = NULL,
+    //         .pCleanupFunc = NULL,
+    //         .pTests = exercise001_tests
+    //     },
+    //     {
+    //         .pName = "2. Get the C version in use",
+    //         .pInitFunc = NULL,
+    //         .pCleanupFunc = NULL,
+    //         .pTests = exercise002_tests
+    //     },
+    //     CU_SUITE_INFO_NULL
+    // });
 
     // Запуск тестов
     CU_basic_set_mode(CU_BRM_VERBOSE);
